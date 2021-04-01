a375fc826f4f351c1a3ada90b41cccf1f3af39d7
MrZerex <nilov-97@mail.ru>
2021-04-01 20:48:26 +0300

    Первое задание

---
+++ Dz1.py
@@ -0,0 +1,15 @@
+number = 11
+print('Число - ', number)
+
+string = 'Someone text'
+print('Строка - ', string)
+
+double = 11.34
+print('Число с плавающей точкой - ', double)
+
+logic = False
+print('Логика - ', logic)
+
+num = int(input('Введите целое число - '))
+str = str(input('Введите текст - '))
+dbl = float(input('Введите число с плавающей точкой - '))
